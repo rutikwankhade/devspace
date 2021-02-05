d60ff021986cc9c145241893302f86dbb7b3d425
rutik wankhade <rutikwankhade2@gmail.com>
2021-02-06 00:22:10 +0530

    add dev.to api

--- src/components/Dev.js
+++ src/components/Dev.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 const Dev = () => {
+
+    const getDevFeed = async () => {
+        const response = await fetch('https://dev.to/api/articles', {
+            headers: {
+                'Content-type': 'application/json',
+            },
+        })
+        const apiResponse = await response.json();
+        console.log(apiResponse)
+
+    }
+
+    useEffect(() => {
+        getDevFeed();
+    })
+
+
     return (
         <div>
             <h1 className="text-2xl font-semibold">Dev</h1>

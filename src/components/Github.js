91a7f43ef7894b3cead2cbe7a93ba4045d7438c0
rutik wankhade <rutikwankhade2@gmail.com>
2021-02-06 14:54:49 +0530

    :sparkles: add github trending api endpoint

--- src/components/Github.js
+++ src/components/Github.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 import githubIcon from '../assets/images/github.png';
 
 const Github = () => {
+    const getTrendingRepos = async () => {
+        const response = await fetch('https://gtrend.yapie.me/repositories?since=daily')
+        const trendingRepos = await response.json();
+        console.log(trendingRepos);
+    };
+
+    useEffect(() => {
+        getTrendingRepos();
+    }, [])
+
     return (
         <div>
             <Header

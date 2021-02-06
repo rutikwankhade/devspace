4c2b9728ea8b69823acbda9a1d3f8a0fad876d9e
rutik wankhade <rutikwankhade2@gmail.com>
2021-02-06 18:05:10 +0530

    :lipstick: style:add ui for repo card

--- src/components/Github.js
+++ src/components/Github.js
@@ -1,17 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import githubIcon from '../assets/images/github.png';
+import starIcon from '../assets/icons/star.svg';
+import forkIcon from '../assets/icons/fork.svg';
+
+
 
 const Github = () => {
+    const [reposList, setReposList] = useState([]);
     const getTrendingRepos = async () => {
         const response = await fetch('https://gtrend.yapie.me/repositories?since=daily')
         const trendingRepos = await response.json();
         console.log(trendingRepos);
+        setReposList(trendingRepos)
     };
 
     useEffect(() => {
         getTrendingRepos();
-    }, [])
+    })
 
     return (
         <div>
@@ -20,6 +26,34 @@ const Github = () => {
                 title='Github'
                 borderColor='border-purple-400'
             />
+            <div className="flex flex-col  justify-center px-8 py-2">
+                <h1 className="text-xl py-1">Trending repositories today</h1>
+                {reposList.map(repo => {
+                    return (
+                        <div className="bg-white  p-4 px-8 rounded-md m-2 text-left">
+                            <a href={repo.url} target="-blank" rel="noreffer">
+                                <h3 className="text-xl font-semibold text-blue-500">{repo.name}</h3>
+                                <span className="text-md">{repo.description}</span>
+                                <div className="flex flex-row my-2">
+                                    <div className="flex mr-2 text-sm">
+                                        <div className="m-1 h-4 w-4 rounded-full"
+                                            style={{ backgroundColor: `${repo.languageColor}` }}></div>
+                                        <span className="m-1">{repo.language}</span>
+                                    </div>
+                                    <div className="flex mr-2">
+                                        <img src={starIcon} alt="star" className="m-1 h-4 w-4" />
+                                        <span>{repo.stars}</span>
+                                    </div>
+                                    <div className="flex mx-2">
+                                        <img src={forkIcon} alt="star" className="m-1 h-4 w-4" />
+                                        <span>{repo.forks}</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 }
